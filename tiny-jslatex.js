87b4a8f53ff1a239af03fe28496307c4c8297999
Juan Latorre <juanlatorre@protonmail.com>
2019-04-09 11:01:26 -0400

    listoco

---
+++ tiny-jslatex.js
@@ -0,0 +1,24 @@
+function process_latex() {
+  $("pre.latex").each(function(e) {
+    var tex = $(this).text();
+    var url =
+      "http://chart.apis.google.com/chart?cht=tx&chl=" +
+      encodeURIComponent(tex);
+    var cls = $(this).attr("class");
+    var img = '<img src="' + url + '" alt="' + tex + '" class="' + cls + '"/>';
+    $(img)
+      .css("display", "block")
+      .insertBefore($(this));
+    $(this).hide();
+  });
+}
+
+$(document).ready(function() {
+  $("#generador").bind("input propertychange", function() {
+    $("#generado").html($("#generador").val());
+  });
+
+  $("#magia").click(function() {
+    process_latex();
+  });
+});
